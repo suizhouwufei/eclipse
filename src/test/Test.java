cf87cea2cbf2d88b4b3c8353f6a5d11dae3d28fc
grady <wushifei666163.com>
2018-12-04 11:35:28 +0800

    create myweb01 11

---
+++ src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("aa");
+		System.out.println("bbb");
+		System.out.println("ccc");
+
+	}
+
+}
